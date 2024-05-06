9067c9ffea0afee3644069ff6285517f492c85b7
tuyizereerneste <tuyizereernest@yahoo.com>
2024-05-06 11:30:45 +0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Find the minimum number of coins required to make up
+a given total amount, given a list of coin denominations.
+"""
+import sys
+
+
+def makeChange(coins, total):
+    """
+    Return:
+        The number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have,
+        return -1
+    """
+    if total <= 0:
+        return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
+
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
